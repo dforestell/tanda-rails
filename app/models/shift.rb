f95660f1372a64ee979a8a76c6161397b1948be0
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-10-16 14:44:26 -0500

    add table for displaying shift details, and logic for hours worked and shift cost

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -10,4 +10,10 @@ class Shift < ApplicationRecord
     def time(time)
         time.strftime('%I:%M%p')
     end
+
+    def hours
+        math = (self.finish - self.start)
+        # divide out seconds and minutes to get result in hours
+        (math / 60 / 60).round(4)
+    end
 end
