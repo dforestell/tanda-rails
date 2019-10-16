952aaaf6a65a9b7d74a1d69c4595c058f59ab2e4
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-10-16 14:21:47 -0500

    add model methods on shift model to formate date and time

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,4 +2,12 @@ class Shift < ApplicationRecord
     belongs_to :user
 
     validates :start, :finish, :user_id, presence: true
+
+    def date
+        self.start.strftime('%m/%d/%Y')    
+    end
+
+    def time(time)
+        time.strftime('%I:%M%p')
+    end
 end
