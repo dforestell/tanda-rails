3348aaef103f150a0e7eccbc7c3a471ed77fff22
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-10-16 18:22:18 -0500

    delete some commented out code

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -20,9 +20,6 @@ class ShiftsController < ApplicationController
             flash[:error] = @shift.errors.full_messages.to_sentence
             redirect_to organization_shifts_path(params[:organization_id])
         end
-
     end
-
-# Parameters: {"utf8"=>"✓", "authenticity_token"=>"1hhVg42oHpV3+qWZEMESdZhzLNPkj+lRDFSFWw3VlBBWK3i7jPisw+Op5eC46f7XkdRpL9ZUvWsac6q6CYPNkQ==", "date(2i)"=>"10", "date(3i)"=>"16", "date(1i)"=>"2019", "start(1i)"=>"2019", "start(2i)"=>"10", "start(3i)"=>"16", "start(4i)"=>"15", "start(5i)"=>"52", "finish(1i)"=>"2019", "finish(2i)"=>"10", "finish(3i)"=>"16", "finish(4i)"=>"20", "finish(5i)"=>"52", "break"=>"25", "commit"=>"Create Shift", "organization_id"=>"2"}
 
 end
