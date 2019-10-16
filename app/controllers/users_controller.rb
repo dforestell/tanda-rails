644f7065e798034644e543474a7ef08a816f725e
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-10-16 18:30:57 -0500

    delete shifts when a user leaves an organization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
     def leave
         current_user.organization_id = nil
         current_user.save(:validate => false)
+        current_user.shifts.destroy_all
         flash[:success] = "You have left the organization"
         redirect_to organizations_path
     end
