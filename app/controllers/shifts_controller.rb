a992c7e4501e70defdffdacb694af6b0a1dcfe29
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-10-17 09:37:52 -0500

    edit and delete shift functionality

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -24,4 +24,32 @@ class ShiftsController < ApplicationController
         end
     end
 
+    def edit
+        @shift = Shift.find(params[:id])
+        @organization = Organization.find(params[:organization_id])
+    end
+
+    def update
+        @shift = Shift.find(params[:id])
+                @date = "#{params['date(3i)']}/#{params['date(2i)']}/#{params['date(1i)']}"
+        @start = @date + " #{params['start(4i)']}:#{params['start(5i)']}"
+        @finish = @date + " #{params['finish(4i)']}:#{params['finish(5i)']}"
+        @shift.start = @start.to_datetime
+        @shift.finish = @finish.to_datetime
+        @shift.break = (params[:break]).to_i
+
+        if @shift.save
+            redirect_to organization_shifts_path(params[:organization_id])
+        else
+            flash[:error] = @shift.errors.full_messages.to_sentence
+            redirect_to organization_shifts_path(params[:organization_id])
+        end
+    end
+
+    def destroy
+        @shift = Shift.find(params[:id])
+        @shift.destroy
+        redirect_to organization_shifts_path(params[:organization_id])
+    end
+
 end
